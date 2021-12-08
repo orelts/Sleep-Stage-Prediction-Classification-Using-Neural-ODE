bc007f565f8326ffa3d42642903ad74c2c58da41
Orel Tsioni <oreltsioni@gmail.com>
2021-12-08 04:08:40 +0200

    data: refined make_dataset.py to load a constant amount of subjects and prepare them as numpy files in a given output_filepath data: added a dataloader.py for the numpy files train: created a dummy loader using mnist and torchdiffeq example

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -4,9 +4,17 @@ import logging
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
 
+from mne.datasets.sleep_physionet.age import fetch_data
+from mne.datasets import sleep_physionet
 import mne
 import matplotlib.pyplot as plt
-
+import numpy as np
+import pandas as pd
+from datetime import datetime
+import math
+import ntpath
+import os
+import dhedfreader
 
 # Data source and documentation https://physionet.org/content/sleep-edfx/1.0.0/
 """
@@ -18,63 +26,193 @@ For the EEG we have few channels recorded depends if its Sleep Telemetry or Slee
 """
 
 
+# Label values
+W = 0
+N1 = 1
+N2 = 2
+N3 = 3
+REM = 4
+UNKNOWN = 5
+
+
+stage_dict = {
+    "W": W,
+    "N1": N1,
+    "N2": N2,
+    "N3": N3,
+    "REM": REM,
+    "UNKNOWN": UNKNOWN
+}
+
+class_dict = {
+    0: "W",
+    1: "N1",
+    2: "N2",
+    3: "N3",
+    4: "REM",
+    5: "UNKNOWN"
+}
+
+ann2label = {
+    "Sleep stage W": 0,
+    "Sleep stage 1": 1,
+    "Sleep stage 2": 2,
+    "Sleep stage 3": 3,
+    "Sleep stage 4": 3,
+    "Sleep stage R": 4,
+    "Sleep stage ?": 5,
+    "Movement time": 5
+}
+
+EPOCH_SEC_SIZE = 30
+
+
+def prepare_physionet(files, output_dir, select_ch="Fpz-Cz"):
+
+    for file in files:
+        psg = file[0]
+        anno = file[1]
+
+        raw = mne.io.read_raw_edf(psg, preload=True, stim_channel=None)
+        sampling_rate = raw.info['sfreq']
+        print(raw.info)
+        raw_ch_df = raw.to_data_frame(scalings=100.0)[select_ch]
+        raw_ch_df = raw_ch_df.to_frame()
+        raw_ch_df.set_index(np.arange(len(raw_ch_df)))
+
+        # Get raw header
+        f = open(psg, 'r', errors='ignore')
+        reader_raw = dhedfreader.BaseEDFReader(f)
+        reader_raw.read_header()
+        h_raw = reader_raw.header
+        f.close()
+        raw_start_dt = datetime.strptime(h_raw['date_time'], "%Y-%m-%d %H:%M:%S")
+
+        # Read annotation and its header
+        f = open(anno, 'r', errors='ignore')
+        reader_ann = dhedfreader.BaseEDFReader(f)
+        reader_ann.read_header()
+        h_ann = reader_ann.header
+        _, _, ann = zip(*reader_ann.records())
+        f.close()
+        ann_start_dt = datetime.strptime(h_ann['date_time'], "%Y-%m-%d %H:%M:%S")
+
+        # Assert that raw and annotation files start at the same time
+        assert raw_start_dt == ann_start_dt
+
+        # Generate label and remove indices
+        remove_idx = []  # indicies of the data that will be removed
+        labels = []  # indicies of the data that have labels
+        label_idx = []
+        for a in ann[0]:
+            onset_sec, duration_sec, ann_char = a
+            ann_str = "".join(ann_char)
+            label = ann2label[ann_str[2:-1]]
+            if label != UNKNOWN:
+                if duration_sec % EPOCH_SEC_SIZE != 0:
+                    raise Exception("Something wrong")
+                duration_epoch = int(duration_sec / EPOCH_SEC_SIZE)
+                label_epoch = np.ones(duration_epoch, dtype=np.int) * label
+                labels.append(label_epoch)
+                idx = int(onset_sec * sampling_rate) + np.arange(duration_sec * sampling_rate, dtype=np.int)
+                label_idx.append(idx)
+
+                print("Include onset:{}, duration:{}, label:{} ({})".format(
+                    onset_sec, duration_sec, label, ann_str
+                ))
+            else:
+                idx = int(onset_sec * sampling_rate) + np.arange(duration_sec * sampling_rate, dtype=np.int)
+                remove_idx.append(idx)
+
+                print("Remove onset:{}, duration:{}, label:{} ({})".format(
+                    onset_sec, duration_sec, label, ann_str))
+        labels = np.hstack(labels)
+
+        print("before remove unwanted: {}".format(np.arange(len(raw_ch_df)).shape))
+        if len(remove_idx) > 0:
+            remove_idx = np.hstack(remove_idx)
+            select_idx = np.setdiff1d(np.arange(len(raw_ch_df)), remove_idx)
+        else:
+            select_idx = np.arange(len(raw_ch_df))
+        print("after remove unwanted: {}".format(select_idx.shape))
+
+        # Select only the data with labels
+        print("before intersect label: {}".format(select_idx.shape))
+        label_idx = np.hstack(label_idx)
+        select_idx = np.intersect1d(select_idx, label_idx)
+        print("after intersect label: {}".format(select_idx.shape))
+
+        # Remove extra index
+        if len(label_idx) > len(select_idx):
+            print("before remove extra labels: {}, {}".format(select_idx.shape, labels.shape))
+            extra_idx = np.setdiff1d(label_idx, select_idx)
+            # Trim the tail
+            if np.all(extra_idx > select_idx[-1]):
+                # n_trims = len(select_idx) % int(EPOCH_SEC_SIZE * sampling_rate)
+                # n_label_trims = int(math.ceil(n_trims / (EPOCH_SEC_SIZE * sampling_rate)))
+                n_label_trims = int(math.ceil(len(extra_idx) / (EPOCH_SEC_SIZE * sampling_rate)))
+                if n_label_trims != 0:
+                    # select_idx = select_idx[:-n_trims]
+                    labels = labels[:-n_label_trims]
+            print("after remove extra labels: {}, {}".format(select_idx.shape, labels.shape))
+
+        # Remove movement and unknown stages if any
+        raw_ch = raw_ch_df.values[select_idx]
+
+        # Verify that we can split into 30-s epochs
+        if len(raw_ch) % (EPOCH_SEC_SIZE * sampling_rate) != 0:
+            raise Exception("Something wrong")
+        n_epochs = len(raw_ch) / (EPOCH_SEC_SIZE * sampling_rate)
+
+        # Get epochs and their corresponding labels
+        x = np.asarray(np.split(raw_ch, n_epochs)).astype(np.float32)
+        y = labels.astype(np.int32)
+
+        assert len(x) == len(y)
+
+        # Select on sleep periods
+        w_edge_mins = 30
+        nw_idx = np.where(y != stage_dict["W"])[0]
+        start_idx = nw_idx[0] - (w_edge_mins * 2)
+        end_idx = nw_idx[-1] + (w_edge_mins * 2)
+        if start_idx < 0: start_idx = 0
+        if end_idx >= len(y): end_idx = len(y) - 1
+        select_idx = np.arange(start_idx, end_idx + 1)
+        print("Data before selection: {}, {}".format(x.shape, y.shape))
+        x = x[select_idx]
+        y = y[select_idx]
+        print("Data after selection: {}, {}".format(x.shape, y.shape))
+
+        # Save
+        filename = ntpath.basename(psg).replace("-PSG.edf", ".npz")
+        save_dict = {
+            "x": x,
+            "y": y,
+            "fs": sampling_rate,
+            "ch_label": select_ch,
+            "header_raw": h_raw,
+            "header_annotation": h_ann,
+        }
+        np.savez(os.path.join(output_dir, filename), **save_dict)
+
+        print("\n=======================================\n")
+
+
 @click.command()
-@click.argument('fname_psg', type=click.Path(exists=True))  # ST7011J0-PSG
-@click.argument('fname_hyp', type=click.Path(exists=True))  # ST7011JP-Hypnogram
 @click.argument('output_filepath', type=click.Path())
 # TODO: load PSG and Hypnogram differently
-def main(fname_psg, fname_hyp, output_filepath):
+def main(output_filepath):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
+    n_subjects = 2
     logger = logging.getLogger(__name__)
-    logger.info('Making final data set from raw data')
-    logger.info(f"Project Directory is set to {project_dir}")
+    logger.info(f'Fetching {n_subjects} subjects from physionet dataset ')
+    sleep_physionet.age.fetch_data(subjects=list(range(n_subjects)))
+    logger.info(f'Trying to read PSG data')
+    files = fetch_data(subjects=[0, 1], recording=[1])
 
-    logger.info(f'Trying to read PSG data file {fname_psg}')
-
-    # Read actual data
-    data_psg = mne.io.read_raw_edf(fname_psg, preload=True)
-    logger.info(f'PSG data fetched {data_psg.info}')
-
-    # Plot
-    logger.info(f'Plotting PSG data {data_psg.info}')
-    data_psg.plot(block=True)
-
-    logger.info(f'Trying to read Hypnogram anotations file {fname_hyp}')
-    data_hyp = mne.read_annotations(fname_hyp)
-
-    # Creating annotiations dict
-    annotation_desc_2_event_id = {'Sleep stage W': 1,
-                                  'Sleep stage 1': 2,
-                                  'Sleep stage 2': 3,
-                                  'Sleep stage 3': 4,
-                                  'Sleep stage 4': 4,
-                                  'Sleep stage R': 5}
-
-    # keep last 30-min wake events before sleep and first 30-min wake events after
-    # sleep and redefine annotations on raw data
-    data_hyp.crop(data_hyp[1]['onset'] - 30 * 60,
-                     data_hyp[-2]['onset'] + 30 * 60)
-    data_psg.set_annotations(data_hyp, emit_warning=False)
-
-    events_train, _ = mne.events_from_annotations(
-        data_psg, event_id=annotation_desc_2_event_id, chunk_duration=30.)
-
-    # create a new event_id that unifies stages 3 and 4
-    event_id = {'Sleep stage W': 1,
-                'Sleep stage 1': 2,
-                'Sleep stage 2': 3,
-                'Sleep stage 3/4': 4,
-                'Sleep stage R': 5}
-
-    # plot events
-    fig = mne.viz.plot_events(events_train, event_id=event_id,
-                              sfreq=data_psg.info['sfreq'],
-                              first_samp=events_train[0, 0])
-
-    # keep the color-code for further plotting
-    stage_colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
+    prepare_physionet(files=files, output_dir=output_filepath)
 
 
 if __name__ == '__main__':
